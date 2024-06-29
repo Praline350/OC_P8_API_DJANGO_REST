afeda1d7fcc92d02850c4352d7331f2943e5cfe9
edwin thierry <edwin.thierry350@gmail.com>
2024-06-29 12:39:15 +0200

    add Project & Contributor Viewset

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from projects.models import Project, Contributor
+from projects.serializers import ProjectListSerializer, ProjectDetailSerializer, ContributorSerializer
+from projects.permissions import IsAuthor, IsContributor
+
+
+class MultipleSerializerMixin:
+
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action in ['retrieve', 'update', 'partial_update', 'destroy'] and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
+
+class ProjectViewset(MultipleSerializerMixin ,ModelViewSet):
+    serializer_class = ProjectListSerializer
+    detail_serializer_class = ProjectDetailSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'create']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['retrieve']:
+            self.permission_classes = [IsAuthenticated, IsContributor]
+        else:  # ['update', 'partial_update', 'destroy']
+            self.permission_classes = [IsAuthenticated, IsAuthor]
+        return super().get_permissions()
+
+    def get_queryset(self):
+        return Project.objects.all()
+    
+
+class ContributoViewset(MultipleSerializerMixin, ModelViewSet):
+    serializer_class = ContributorSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return Contributor.objects.all()
