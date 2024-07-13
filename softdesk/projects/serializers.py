a4c3d29a4a5a4d9b945d195737b18a1f5db6f8cf
edwin thierry <edwin.thierry350@gmail.com>
2024-07-13 11:57:08 +0200

    Issues Serialiazer

--- softdesk/projects/serializers.py
+++ softdesk/projects/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
 from projects.models import Project, Contributor, Issue, Comment
@@ -17,23 +18,33 @@ class ContributorSerializer(serializers.ModelSerializer):
 
 class ProjectListSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
-    contributors = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, required=False)
+    contributors = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        many=True,
+        slug_field='username',
+        required=False
+    )
 
     class Meta:
         model = Project
         fields = ['id', 'name', 'description', 'type', 'author', 'contributors', 'created_time']
 
     def create(self, validated_data):
-        contributors_data = validated_data.pop('contributors', [])
+        contributors_usernames = validated_data.pop('contributors', [])
         request = self.context.get('request')
         project = Project.objects.create(author=request.user, **validated_data)
-        # Ajouter l'auteur comme contributeur et supprimer les doublons
-        unique_contributors = set(contributors_data)
-        unique_contributors.add(project.author)
-        for user in unique_contributors:
-            Contributor.objects.create(user=user, project=project)
+        # Ajouter l'auteur comme contributeur
+        Contributor.objects.create(user=request.user, project=project)
+        
+       # Ajouter les autres contributeurs en vérifiant les doublons
+        for username in set(contributors_usernames):
+            user = User.objects.get(username=username)
+            if not Contributor.objects.filter(user=user, project=project).exists():
+                Contributor.objects.create(user=user, project=project)
+                
         return project
-    
+        
+
 
 class ProjectDetailSerializer(ProjectListSerializer):
     class Meta(ProjectListSerializer.Meta):
@@ -43,21 +54,52 @@ class ProjectDetailSerializer(ProjectListSerializer):
         request = self.context.get('request')
         if instance.author != request.user:
             raise serializers.ValidationError("Vous n'avez pas les permissions")
-        contributors_data = validated_data.pop('contributors', [])
+        
+        contributors_usernames = validated_data.pop('contributors', [])
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
         instance.type = validated_data.get('type', instance.type)
         instance.save()
-        if contributors_data is not None:
-            new_contributors = set([user.id for user in contributors_data])
-            new_contributors.add(request.user.id)
-            current_contributors = set(instance.contributors.values_list('id', flat=True))
+
+        if contributors_usernames:
+            new_contributors = set(contributors_usernames)
+            new_contributors.add(request.user.username)
+            current_contributors = set(instance.contributors.values_list('username', flat=True))
             contributors_to_add = new_contributors - current_contributors
             contributors_to_remove = current_contributors - new_contributors
-            for user_id in contributors_to_add:
-                user = User.objects.get(id=user_id)
+            
+            for username in contributors_to_add:
+                user = User.objects.get(username=username)
                 Contributor.objects.create(user=user, project=instance)
-            for user_id in contributors_to_remove:
-                Contributor.objects.filter(user_id=user_id, project=instance).delete()
-            return instance
+            
+            for username in contributors_to_remove:
+                user = User.objects.get(username=username)
+                Contributor.objects.filter(user=user, project=instance).delete()
+        
+        return instance
 
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.user.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'issue', 'author', 'description', 'created_time']
+
+
+class IssueListSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.user.username')
+    project = serializers.ReadOnlyField(source='project.name')
+
+    
+
+    class Meta:
+        model = Issue
+        fields = ['id', 'project', 'author', 'title', 'description', 'status', 'priority', 'created_time', 'updated_time']
+        read_only_fields = ['created_time', 'updated_time']
+
+
+class IssueDetailSerializer(IssueListSerializer):
+    class Meta(IssueListSerializer.Meta):
+        fields = IssueListSerializer.fields 
