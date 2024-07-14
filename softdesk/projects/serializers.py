c879b1cffc713f5ff6e4bdc3c276bedb2a714364
edwin thierry <edwin.thierry350@gmail.com>
2024-07-14 12:48:16 +0200

    Comment serializer

--- softdesk/projects/serializers.py
+++ softdesk/projects/serializers.py
@@ -29,19 +29,30 @@ class ProjectListSerializer(serializers.ModelSerializer):
         model = Project
         fields = ['id', 'name', 'description', 'type', 'author', 'contributors', 'created_time']
 
+
+    def validate_contributors(self, value):
+        # Remove duplicates from the contributors list
+        unique_contributors = set(value)
+        
+        # Check if author is in the contributors list and raise a validation error if needed
+        request = self.context.get('request')
+        if request and request.user in unique_contributors:
+            unique_contributors.remove(request.user)
+
+        return list(unique_contributors)
+
     def create(self, validated_data):
         contributors_usernames = validated_data.pop('contributors', [])
         request = self.context.get('request')
-        project = Project.objects.create(author=request.user, **validated_data)
-        # Ajouter l'auteur comme contributeur
-        Contributor.objects.create(user=request.user, project=project)
         
-       # Ajouter les autres contributeurs en vérifiant les doublons
+        # Create project
+        project = Project.objects.create(author=request.user, **validated_data)
+        
+        # Add other contributors
         for username in set(contributors_usernames):
             user = User.objects.get(username=username)
-            if not Contributor.objects.filter(user=user, project=project).exists():
-                Contributor.objects.create(user=user, project=project)
-                
+            Contributor.objects.get_or_create(user=user, project=project)
+        
         return project
         
 
@@ -82,24 +93,45 @@ class ProjectDetailSerializer(ProjectListSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.user.username')
+    issue = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Comment
         fields = ['id', 'issue', 'author', 'description', 'created_time']
+        read_only_fields = ['created_time', 'author']
 
 
 class IssueListSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.user.username')
     project = serializers.ReadOnlyField(source='project.name')
-
-    
+    assignees = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, write_only=True, required=False)
 
     class Meta:
         model = Issue
-        fields = ['id', 'project', 'author', 'title', 'description', 'status', 'priority', 'created_time', 'updated_time']
+        fields = ['id', 'project', 'author', 'title', 'description', 'status', 'priority', 'created_time', 'updated_time', 'assignees']
         read_only_fields = ['created_time', 'updated_time']
 
+    def validate_assignees(self, value):
+        project = self.instance.project
+        if isinstance(project, int):
+            project = Project.objects.get(pk=project)
+        for user in value:
+            if not Contributor.objects.filter(project=project, user=user).exists():
+                raise serializers.ValidationError(f"{user.username} is not a contributor of the project.")
+        return value
+    
+    def create(self, validated_data):
+        assignees = validated_data.pop('assignees', [])
+        issue = super().create(validated_data)
+        for user in assignees:
+            issue.assignees.add(Contributor.objects.get(project=issue.project, user=user))
+        return issue
+    
+    def update(self, instance, validated_data):
+        assignees = validated_data.pop('assignees', None)
+        if assignees is not None:
+            instance.assignees.set([Contributor.objects.get(project=instance.project, user_id=user_id) for user_id in assignees])
+        return super().update(instance, validated_data)
 
-class IssueDetailSerializer(IssueListSerializer):
-    class Meta(IssueListSerializer.Meta):
-        fields = IssueListSerializer.fields 
+
+    
